0f504afe0161136a37305407fff1b6a9042fcd8c
Johannes Blaschke <jpblaschke@lbl.gov>
2018-03-20 17:33:42 -0700

    working on travis: figuring out why spinx won't build

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -11,10 +11,10 @@ set -e # Exit with nonzero exit code if anything fails
 #TARGET_BRANCH="gh-pages"
 
 # Pull requests and commits to other branches shouldn't try to deploy
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy."
-    exit 0
-fi
+#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#    echo "Skipping deploy."
+#    exit 0
+#fi
 
 # Save some useful information
 #REPO=`git config remote.origin.url`
